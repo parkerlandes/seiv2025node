025318fab2535471c5f8f6ef8c0c04a7bbe160d7
SleepiestFool01 <teaganm856@gmail.com>
2025-10-03 21:27:10 -0500

    update backend filter

--- app/routes/course.routes.js
+++ app/routes/course.routes.js
@@ -3,10 +3,24 @@ module.exports = app => {
   var router = require("express").Router();
   // Create a new course
   router.post("/", courses.create);
+
+  //filters for the courses
+
   // Retrieve all courses
   router.get("/", courses.findAll);
   // Retrieve a single course with id
   router.get("/:id", courses.findOne);
+  // Retrieve courses by hours
+  router.get("/h/:hours", courses.findByHours);
+  //retrieve courses by courseId
+  router.get("/c/:courseNum", courses.findByCourseNum);
+  //retrieve courses by name 
+  router.get("/n/:name", courses.findByName);
+  //retreive courses by dept
+  router.get("/d/:dept", courses.findByDept);
+  //retrieve courses by level
+  router.get("/l/:courseLevel", courses.findByLevel);
+
   // Update a course with id
   router.put("/:id", courses.update);
   // Delete a course with id
